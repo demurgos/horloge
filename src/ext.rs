586cbf54bcb8751086cc647802c5f1589b48d302
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2025-06-05 01:19:01 +0000

    feature: add `ClockExt::saturating_duration_until`

--- src/ext.rs
+++ src/ext.rs
@@ -8,8 +8,14 @@ use std::ops::Sub;
 
 pub trait ClockExt: Clock + private::SchedulerExtSealed {
   /// Returns the amount of time elapsed from `earlier` to now, or zero duration
-  /// if that instant is later than this one.
+  /// if that instant is later than the current time.
   fn saturating_duration_since(&self, earlier: Self::Instant) -> <Self::Instant as Sub<Self::Instant>>::Output
+  where
+    Self::Instant: Sub<Self::Instant, Output: Duration>;
+
+  /// Returns the amount of time from now to `later`, or zero duration
+  /// if that instant is earlier than the current time.
+  fn saturating_duration_until(&self, later: Self::Instant) -> <Self::Instant as Sub<Self::Instant>>::Output
   where
     Self::Instant: Sub<Self::Instant, Output: Duration>;
 }
@@ -29,6 +35,22 @@ where
       return <<Self::Instant as Sub<_>>::Output as Duration>::zero();
     }
     let duration = now - earlier;
+    let zero = <<Self::Instant as Sub<_>>::Output as Duration>::zero();
+    if duration <= zero {
+      return zero;
+    }
+    duration
+  }
+
+  fn saturating_duration_until(&self, later: Self::Instant) -> <Self::Instant as Sub<Self::Instant>>::Output
+  where
+    Self::Instant: Sub<Self::Instant, Output: Duration>,
+  {
+    let now = self.now();
+    if later <= now {
+      return <<Self::Instant as Sub<_>>::Output as Duration>::zero();
+    }
+    let duration = later - now;
     let zero = <<Self::Instant as Sub<_>>::Output as Duration>::zero();
     if duration <= zero {
       return zero;
